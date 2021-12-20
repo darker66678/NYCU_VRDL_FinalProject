f0eac051cfc42707466bd3d1eedf90d8ac29dcca
Chialiang86 <a930039930039@gmail.com>
2021-12-20 12:25:43 +0800

    There may be more than one label in an image, so add some id-check code to yolo_label.py

--- yolo_label.py
+++ yolo_label.py
@@ -1,7 +1,6 @@
 import os
 import pandas as pd
 
-
 def convert(size, box):
     dw = size[0]
     dh = size[1]
@@ -15,26 +14,31 @@ def convert(size, box):
     h = h/dh
     return (x, y, w, h)
 
-
+id_cache = None # modified by chialiang
 out_path = r'./data/labels/train/'
 data = pd.read_csv(r'./data/stage_2_train_labels.csv')
 for i in range(len(data)):
     target = data['Target'].iloc[i]
+    patientId = data['patientId'].iloc[i] # modified by chialiang
+
+    # modified by chialiang
     if(target):
-        patientId = data['patientId'].iloc[i]
+        if data['patientId'].iloc[i] != id_cache:
+            id_cache = data['patientId'].iloc[i]
+            txt_outfile = open(out_path+patientId+'.txt', "w")
         x = data['x'].iloc[i]
         y = data['y'].iloc[i]
         w = data['width'].iloc[i]
         h = data['height'].iloc[i]
         yolo_x, yolo_y, yolo_w, yolo_h = convert([1024, 1024], [x, y, w, h])
         bbox = [float(yolo_x), float(yolo_y), float(yolo_w), float(yolo_h)]
-        txt_outfile = open(out_path+patientId+'.txt', "w")
         txt_outfile.write(
             str(0) + " " + " ".join([str(i) for i in bbox]) + '\n')
 
 txt_outfile = open('./data/train.txt', "w")
 for i in range(len(data)):
     patientId = data['patientId'].iloc[i]
-    txt_outfile.write( '../train/'+patientId+'.png' +'\n')
+    # relative to the path you exective train.py of yolov5
+    txt_outfile.write( '../data/imgs/train/'+patientId+'.png' +'\n')
 
 print("finish!")
